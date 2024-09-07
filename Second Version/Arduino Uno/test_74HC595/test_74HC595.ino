d5f2e42d07c6a1e5eafe0c7260aaed3619578991
bar <bar5132@gmail.com>
2024-09-07 15:39:03 +0300

    working 2 chained 74HC595 with 16 leds

--- Second Version/Arduino Uno/test_74HC595/test_74HC595.ino
+++ Second Version/Arduino Uno/test_74HC595/test_74HC595.ino
@@ -3,36 +3,33 @@
 #define pinLatch 3
 #define pinClk 4
 
+uint16_t digit = 1;
 
 
 void setup() {
-  
-
   pinMode(pinClk, OUTPUT);
   pinMode(pinSer, OUTPUT);
   pinMode(pinLatch, OUTPUT);
-
- //byte digit = 0b00100000;
-byte digit = 0b10000000;
-  
-  digitalWrite(pinLatch, LOW);   
-  
-   for (int i = 0; i < 8; i++)
-   {
-    delay(20);
-     digitalWrite(pinSer,  bitRead(digit,i));   
-     delay(20);
-     digitalWrite(pinClk, HIGH);   
-     delay(20);
-     digitalWrite(pinClk, LOW);
-     
-   } 
-   digitalWrite(pinLatch, HIGH);
-   
 
 }
 
 void loop() {
  
+  delay(100);
+  digit = (digit << 1) | (digit >> 15); // 16 - 1 = 15 (from Q8 to Q0) 
+  //digit = (digit >> 1) | (digit << 15); // 16 - 1 = 15 (from Q0 to Q8)
+  updateLeds(digit);
+  
 
+}
+
+void updateLeds(uint16_t digit)
+{
+  digitalWrite(pinLatch, LOW);   
+
+  //for two chained 74HC595 we use twice the shiftOut to send two bytes
+  shiftOut(pinSer, pinClk, LSBFIRST, digit);
+  shiftOut(pinSer, pinClk, LSBFIRST, digit>>8);
+  
+  digitalWrite(pinLatch, HIGH);
 }
